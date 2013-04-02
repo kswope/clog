b8361854617517dbb91b87535f6ecfbd8357acf8
kswope <git-kevdev@snkmail.com>
2013-04-01 21:20:59 -0400

    Added new test rails 3

---
+++ test/rails_root/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsRoot::Application.config.secret_token = 'bc39325dd57be24f919be2165bd871a66c7c0c94bc74d7959f0c1df1f3ba05e53e786b645fb5a4ce7fc8ffb53ec2f99001d2aed8d58edc030642f922c735f158'
