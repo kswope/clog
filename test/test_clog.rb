1f11c591efc208166cabcae2822a83ff93e2a6ba
kswope <git-kevdev@snkmail.com>
2013-04-01 20:36:25 -0400

    Polished up visible tests.

--- test/test_clog.rb
+++ test/test_clog.rb
@@ -1,16 +1,17 @@
 require 'helper'
-require 'redgreen'
 
 class Testclog < Test::Unit::TestCase
+
+
 
   def test_format
 
     # These colors will probably be default
-    Clog.colors(:red, :white)  
+    # Clog.colors(:red, :white)  
 
     clog # should show flag
 
-    clog nil
+    clog nil # prints out a literal nil
 
     clog "Single short string argument"
     clog "Single long string - Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
@@ -18,10 +19,11 @@ class Testclog < Test::Unit::TestCase
     clog :myvar, "Symbol with short value"
     clog :myvar, "Symbol with long string, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
+    # new two should have a value on each line
     clog "Multiple", "String", "Arguments", "Here"
     clog 1, Object.new, Kernel, RUBY_VERSION, :a_symbol
     
-    # hash dump
+    # this doesn't show much new does it?
     {:one=>1, :two=>2, :three=>3, :four=>4, :five=>5}.each do |key, value|
       clog key, value
     end
@@ -32,7 +34,10 @@ class Testclog < Test::Unit::TestCase
 
   end
 
+
+
   def test_colors
+
 
     def runner(bg, fg=nil)
 
@@ -42,8 +47,10 @@ class Testclog < Test::Unit::TestCase
         Clog.colors(bg)
       end
       
-      clog "#{fg} on #{bg}"
+      clog "#{fg || 'default'} on #{bg}"
+
     end
+
     
     runner :black
     runner :red
